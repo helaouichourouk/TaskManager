84af2e6aee227e95c422ba4b6a0504f44d9ee785
Chourouk Helaoui <chourouk.helaoui2@gmail.com>
2024-12-21 22:13:18 +0100

    secure dbconnection

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
+    const mongoURI = process.env.MONGO_URI; // Lire l'URI depuis les variables d'environnement
+
+    if (!mongoURI) {
+        throw new Error('MongoDB URI is not defined in environment variables.');
+    }
+
     if (mongoose.connection.readyState === 0) {
         try {
-            await mongoose.connect('mongodb+srv://admin:mETLsVwLOKTsdwLQ@cluster0.g2qdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+            await mongoose.connect(mongoURI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            } as mongoose.ConnectOptions);
             console.log('MongoDB connected');
         } catch (error) {
             console.error('MongoDB connection error:', error);
